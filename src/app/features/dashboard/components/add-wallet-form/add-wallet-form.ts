a3d8bac9b05843007b3fd3a810985e27cb464d9d
Tonu Cristian <cristian.tonu@iongroup.com>
2026-06-17 15:15:43 +0300

    feat: add wallet form validation wip

--- src/app/features/dashboard/components/add-wallet-form/add-wallet-form.ts
+++ src/app/features/dashboard/components/add-wallet-form/add-wallet-form.ts
@@ -1,16 +1,19 @@
 import { Component, model, signal } from '@angular/core';
 import { Button } from '../../../../shared/ui/button/button';
-import { form, FormField, FormRoot } from '@angular/forms/signals';
+import { form, FormField, FormRoot, maxLength, min, required } from '@angular/forms/signals';
+import { CURRENCY } from '../../dashboard.models';
+import { CURRENCY_OPTIONS } from '../../dashboard.constants';
 
 const ADD_WALLET_FORM_INPUTS = [
   { type: 'text', name: 'name', label: 'Name' },
   { type: 'text', name: 'currency', label: 'Currency' },
-  { type: 'number', name: 'balance', label: 'Balance' },
+  { type: 'text', name: 'balance', label: 'Balance' },
 ];
+
 
 interface AddWalletFormModel {
   name: string;
-  currency: string;
+  currency: CURRENCY;
   balance: number;
 }
 
@@ -25,13 +28,22 @@ export class AddWalletForm {
 
   public addWalletModel = signal<AddWalletFormModel>({
     name: '',
-    currency: 'EUR',
+    currency: 'eur',
     balance: 0,
   });
 
   public readonly addWalletFormInputs = ADD_WALLET_FORM_INPUTS;
+  public readonly currencyOptions = CURRENCY_OPTIONS;
 
-  public addWalletForm = form(this.addWalletModel, {
+  public addWalletForm = form(this.addWalletModel, (schemaPath) => {
+    required(schemaPath.name, { message: "Name field is required!" });
+    maxLength(schemaPath.name, 60, { message: 'The name is too long!' });
+    
+    required(schemaPath.currency, { message: "Currency field is required!" });
+
+    required(schemaPath.balance, { message: "Balance field is required!" });
+    min(schemaPath.balance, 0, { message: 'Balance must be at least 0!' });
+  }, {
     submission: {
       action: this.submitForm,
     },
