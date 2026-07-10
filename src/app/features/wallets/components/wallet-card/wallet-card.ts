217c265b03dc8512c91df6cb0293876eddef879d
Tonu Cristi <chrisdev2002@gmail.com>
2026-07-10 10:44:28 +0300

    feat: wallet card wip

---
+++ src/app/features/wallets/components/wallet-card/wallet-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-card',
+  imports: [],
+  templateUrl: './wallet-card.html',
+  styleUrl: './wallet-card.css',
+})
+export class WalletCard {}
--- src/app/features/wallets/wallets.ts
+++ src/app/features/wallets/wallets.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+
 import { PageTitle } from '../../shared/ui/page-title/page-title';
+import { CombinedBalance } from './components/combined-balance/combined-balance';
+import { WalletCard } from './components/wallet-card/wallet-card';
 
 @Component({
   selector: 'app-wallets',
-  imports: [PageTitle],
+  imports: [PageTitle, CombinedBalance, WalletCard],
   templateUrl: './wallets.html',
   styleUrl: './wallets.css',
 })
